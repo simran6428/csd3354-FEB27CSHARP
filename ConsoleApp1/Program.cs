1b5c547bc19875d757b96aef80ff0d15128cc436
visualstudio <visualstudio@122-PC17>
2019-02-27 09:53:20 -0500

    m,

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var Orderprocessor = new Orderprocessor();
-            var Order = new Order { Dateplaced = DateTime.Now, Totalprice = 100f };
-            Orderprocessor.Process(Order);
+            var Orderprocessor = new orderprocessor();
+            var Order = new Order { Dateplaced = DateTime.Now, TotalPrice = 100f };
+            Orderprocessor.process(Order);
         }
     }
+
+
+
+    public class orderprocessor
+    {
+        private readonly ShippingCalculator _ShippingCalculator;
+        public orderprocessor()
+        {
+            _ShippingCalculator = new ShippingCalculator();
+
+        }
+        public void process(Order order)
+        {
+            if (order.Isshipped)
+                throw new InvalidOperationException("this order is already processed.");
+            order.Shipment = new Shipment
+            {
+                Cost = _ShippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1);
+        }
+    }
+
+
+    public class ShippingCalculator
+    {
+        public float CalculatingShipping(Order order)
+        {
+            if (order.TotalPrice < 30f) return order.TotalPrice * 0.1f;
+            return 0;
+        }
+    }
+
 }
+
